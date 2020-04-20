b069785b742d082b8344a9e4cfc1a6b4dad67eeb
ailchenko <ailchenko@solit-clouds.ru>
2020-04-20 23:40:19 +0300

    Добавлены комментарии

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import { routes } from './routing-config';
 
 /**
- * //
+ * Модуль маршрутизации
  */
 @NgModule({
     imports: [
--- src/app/routing/routing-config.ts
+++ src/app/routing/routing-config.ts
@@ -1,21 +1,29 @@
 import { Routes } from '@angular/router';
 
 /**
- * //
+ * Конфигурация маршрутизации
  */
 export const routes: Routes = [
+    /** Абстрактный базовый маршрут */
     {
         path: '',
         redirectTo: 'dashboard',
         pathMatch: 'full',
         children: [
+            /** Страница логина */
             {
                 path: 'login',
                 loadChildren: null
-            }, {
+            },
+
+            /** Страница рабочего стола */
+            {
                 path: 'dashboard',
                 loadChildren: null
-            }, {
+            },
+            
+            /** Страница 404 */
+            {
                 path: '**',
                 loadChildren: null 
             }
