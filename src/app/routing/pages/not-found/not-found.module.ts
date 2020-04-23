e9751eb8a57efcb0d7922bf24d7a116cbbec2aeb
ailchenko <ailchenko@solit-clouds.ru>
2020-04-23 19:24:38 +0300

    Добавлена компонент для страницы 404

---
+++ src/app/routing/pages/not-found/not-found.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { NotFoundPageComponent } from './not-found-page.component';
+
+/**
+ * //
+ */
+@NgModule({
+    imports: [],
+    declarations: [
+        NotFoundPageComponent
+    ]
+})
+export class NotFoundModule {}
